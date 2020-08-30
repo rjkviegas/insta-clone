58d0efb01d555def6d0aaa2e15ade714b1718d75
Ryan Viegas <rjkviegas@gmail.com>
2020-08-30 10:50:48 +0100

    Test Account validation should have a username

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :account do
-    
+      username { 'Test Johnson' }
+      first_name { "Johnson" }
+      last_name  { "Doe" }
+      email { 'testjohnson@testmail.com' }
+      password { '123456' }
   end
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -7,17 +7,25 @@ RSpec.describe Account, type: :model do
       account.username = nil
       expect(account).to_not be_valid
     end
-    it 'should have a first name' do
-      account.first_name = nil
-      expect(account).to_not be_valid
-    end
-    it 'should have a last name' do
-      account.last_name = nil
-      expect(account).to_not be_valid
-    end
-    it 'should have an email' do
-      account.email = nil
-      expect(account).to_not be_valid
-    end
+    # it 'username length <= 20 chars' do
+    #   account.username = 'TwentyPlusCharacterUserName'
+    #   expect(account).to_not be_valid
+    # end
+    # it 'should have a first name' do
+    #   account.first_name = nil
+    #   expect(account).to_not be_valid
+    # end
+    # it 'first name length <= 20 chars' do
+    #   account.first_name = 'Terry'
+    #   expect(account).to be_valid
+    # end
+    # it 'should have a last name' do
+    #   account.last_name = nil
+    #   expect(account).to_not be_valid
+    # end
+    # it 'should have an email' do
+    #   account.email = nil
+    #   expect(account).to_not be_valid
+    # end
   end
 end
