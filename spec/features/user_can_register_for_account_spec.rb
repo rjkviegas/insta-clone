ebb1e7bfbe61fc85b5f5d67816fa55bdef99ff9f
Ryan Viegas <rjkviegas@gmail.com>
2020-08-30 10:17:38 +0100

    Feature Test User registration account creation, added username, first_name and last_name

--- spec/features/user_can_register_for_account_spec.rb
+++ spec/features/user_can_register_for_account_spec.rb
@@ -20,4 +20,20 @@ RSpec.feature 'User registration' do
     expect(page).to have_field('account[password_confirmation]')
   end
 
+  scenario 'account creation' do
+    visit '/accounts/sign_up'
+    fill_in 'account[username]', with: 'testname'
+    fill_in 'account[first_name]', with: 'test'
+    fill_in 'account[last_name]', with: 'name'
+    fill_in 'account[email]', with: 'test@test.com'
+    fill_in 'account[password]', with: '123456'
+    fill_in 'account[password_confirmation]', with: '123456'
+    click_button 'Sign up'
+    expect(Account.count).to eq(1)
+    expect(Account.first.username).to eq('testname')
+    expect(Account.first.first_name).to eq('test')
+    expect(Account.first.last_name).to eq('name')
+    expect(Account.first.email).to eq('test@test.com')
+  end
+
 end
