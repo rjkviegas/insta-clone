dac826fd32743948e2ecdd6bdab19b636a1f77e2
Ryan Viegas <rjkviegas@gmail.com>
2020-08-31 14:15:19 +0100

    Post validation needs an account id

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    let(:post) { FactoryBot.build(:post) }
+    it 'needs an account_id' do
+      post.account_id = nil
+      expect(post).to_not be_valid
+    end
+  end
 end
