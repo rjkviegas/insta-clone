7d106f3e9df9c50ffc1efcc68fc02eaf79f38838
Ryan Viegas <rjkviegas@gmail.com>
2020-08-31 15:41:14 +0100

    Feature Test alert if post not created

--- spec/features/create_post_spec.rb
+++ spec/features/create_post_spec.rb
@@ -9,5 +9,13 @@ RSpec.feature 'Creating posts' do
     click_button 'Create Post'
     expect(page).to have_content('#coffeetime')
     expect(page).to have_css("img[src*='coffee.jpg']")
+    expect(page).to have_content('Post was created successfully')
+  end
+  scenario 'alert if post not created' do
+    sign_up
+    click_link 'New Post'
+    fill_in 'post[caption]', with: 'nom nom nom #coffeetime' 
+    click_button 'Create Post'
+    expect(page).to have_content('Post was not saved!')
   end
 end
